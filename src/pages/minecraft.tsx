ecb1e68889a6d9f4254a6508ef7af60f3a1979f4
Corey Laird <me@coreylaird.com>
2023-10-25 16:52:07 -0700

    Make API path relative

--- src/pages/minecraft.tsx
+++ src/pages/minecraft.tsx
@@ -6,7 +6,7 @@ const MinecraftEmbed = () => (
     <div className="page-interior page-interior--stretch">
       <iframe
         className="full-embed full-embed--stretch"
-        src="https://www.falconsyndicate.net/minecraft/map"
+        src="/dynmap"
       ></iframe>
     </div>
   </Layout>
