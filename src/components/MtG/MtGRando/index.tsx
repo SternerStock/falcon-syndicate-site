dd58fcf223efbb39edbd0fc61235a3c415de7276
Corey Laird <me@coreylaird.com>
2023-11-03 23:18:38 -0700

    Add additional formats PreDH and Premodern

--- src/components/MtG/MtGRando/index.tsx
+++ src/components/MtG/MtGRando/index.tsx
@@ -160,10 +160,17 @@ class MtGRando extends React.Component<{}, MtGRandoState> {
       },
       {
         deckType: 'Commander',
-        name: 'Pauper',
+        name: 'Pauper Commander',
         deckSize: 99,
         allowSilver: true,
-        rulesUrl: 'https://www.pdhhomebase.ca/rules',
+        rulesUrl: 'https://pdhhomebase.com/rules/',
+      },
+      {
+        deckType: 'Commander',
+        name: 'PreDH',
+        deckSize: 99,
+        allowSilver: true,
+        rulesUrl: 'https://articles.starcitygames.com/magic-the-gathering/commander-sub-format-predh-is-the-new-magic-rage/',
       },
       {
         deckType: 'Commander',
@@ -198,6 +205,21 @@ class MtGRando extends React.Component<{}, MtGRandoState> {
       },
       {
         deckType: 'Normal MtG',
+        name: 'Premodern',
+        deckSize: 60,
+        allowSilver: false,
+        rulesUrl:
+          'https://premodernmagic.com/rules#rules',
+      },
+      {
+        deckType: 'Normal MtG',
+        name: 'Pauper',
+        deckSize: 60,
+        allowSilver: false,
+        rulesUrl: 'https://mtg.fandom.com/wiki/Pauper',
+      },
+      {
+        deckType: 'Normal MtG',
         name: 'Pioneer',
         deckSize: 60,
         allowSilver: false,
@@ -219,14 +241,6 @@ class MtGRando extends React.Component<{}, MtGRandoState> {
         allowSilver: true,
         rulesUrl:
           'http://magic.wizards.com/en/game-info/gameplay/formats/vintage',
-      },
-      {
-        deckType: 'Normal MtG',
-        name: 'Pauper',
-        deckSize: 60,
-        allowSilver: true,
-        rulesUrl:
-          'http://magic.wizards.com/en/game-info/gameplay/formats/pauper',
       },
       {
         deckType: 'Normal MtG',
